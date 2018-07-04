93b852f61d91bede661a81749eac980db0c81422
ddeniz001 <38838004+ddeniz001@users.noreply.github.com>
2018-07-04 17:51:34 -0400

    Add files via upload

---
+++ budgetClasses.py
@@ -0,0 +1,87 @@
+# This program is written for the sole purpose of teaching you to be smarter with your budget.
+
+from enum import Enum
+
+# ------------------------------------------- *** EXPENSE CLASS *** ---------------------------
+
+# Main menu function for the program. Enumeration is used for convenience. (Will be added later.)
+
+
+class menuOptions(Enum):
+    SUM_UP_INCOME = 1
+    DELETE_INCOME = 2
+    LIST_INCOME = 3
+    ADD_INCOME = 4
+    ADD_EXPENSE = 5
+    LIST_EXPENSE = 6
+    DEL_EXPENSE = 7
+    SUM_EXPENSES = 8
+    GET_NET = 9
+    EXIT = 10
+
+# Expense options for input. Given values so that the user can type it in themselves.
+
+
+class ExpenseCategory(Enum):  # Parent Class
+    groceries = 1
+    entertainment = 2
+    memberships = 3
+    utilities = 4
+    rent = 5
+    other = 6
+
+
+class Expense:
+    def __init__(self, category):
+        self.category = category
+        self.price = 0
+        self.add_expense()
+        self.print_expense()
+        self.expense = 0
+
+    def add_expense(self):
+        self.price = int(input('Enter ' + self.category.name + " price:"))
+
+    def print_expense(self):
+        print("The price is, ", self.price)
+
+    def __str__(self):
+        return self.category.name.capitalize() + " Price: $" + str(self.price)
+
+    def expenseSum(self):
+        return "Expense sum is:" + self.category.price
+
+    def getCategory(self):
+        return self.category.name.capitalize() + " Expense Amount: $" + str(self.price)
+
+
+# ---------------------------------------------*** INCOME CLASS ***-----------------------
+# Input options for user.
+class IncomeCategory(Enum):
+    job = 1
+    allowance = 2
+    loan = 3
+    other = 4
+    scholarship = 5
+
+
+class Income:
+    def __init__(self, category, amount=None):
+        self.category = category
+        self.amount = amount
+        if (amount == None):
+            self.add_income()
+        self.print_income()
+
+    def __str__(self):
+        return + self.category.name.capitalize() + "category. Amount: $" + str(self.amount)
+
+    def add_income(self):
+        self.amount = int(
+            input("Enter " + self.category.name + " income amount:"))
+
+    def print_income(self):
+        print("The amount is $", self.amount)
+
+    def getCategory(self):
+        return self.category.name.capitalize() + " Income Amount: $" + str(self.amount)
---
+++ budgetPlanner.py
@@ -0,0 +1,123 @@
+# This program is written for the sole purpose of teaching you to be smarter with your budget.
+
+from enum import Enum
+
+import budgetClasses
+
+
+# ----------------------------------------------*** CODE GOES HERE ***----------------------------------------------
+expenselst = []
+income_list = []
+income_amount = []
+# 1. Main menu; User picks an option.
+while True:
+    Options = input(
+        """Welcome to Budget Planner. What would you like to do?\n
+        1 - Add expense\n
+        2 - Add income \n
+        3 - List incomes \n
+        4 - List expense\n
+        5 - Sum up incomes\n
+        6 - Sum up expenses\n
+        7 - Delete expense\n
+        8 - Delete income\n
+        9 - Get your net\n
+        10 - Go and **** yourself\n""")
+# 2. Add Expense
+    if Options == "1":
+        inp_1 = input("Add Expense? [yes/no]: ")
+        if inp_1 == "yes":
+            while True:
+                try:
+                    expenselst.append(
+                        Expense(ExpenseCategory[input("Enter category name from the list below.\n"
+                                                      "1 - groceries\n2 - entertainment\n3 - memberships\n4 - utilities\n5 - rent\n6 - other")]))
+                    break
+                except KeyError:
+                    print("Please Enter Valid Input!")
+        elif inp_1 == "no":
+            break
+        else:
+            print("Enter a valid answer!")
+
+# 3. Add Income
+    elif Options == "2":
+        inp_6 = input("Add Income? [yes/no]: ")
+        if inp_6 == "yes":
+            while True:
+                try:
+                    income_list.append(
+                        Income(IncomeCategory[input("Type in the category name from the list below.\n"
+                                                    "1 - job\n2 - allowance\n3 - loan\n4 - other\n5 - scholarship\n")]))
+                    break
+                except KeyError:
+                    print("Please Enter Valid Input!")
+        elif inp_6 == "no":
+            break
+        else:
+            print("Enter a valid answer!!")
+        input_3 = input("Delete an income item? [yes/no]: ")
+# 4. List Income
+    elif Options == "3":
+        for i in income_list:
+            print(str(income_list.index(i)) + " " + i.getCategory())
+# 5. List Expense
+    elif Options == "4":
+        for i in expenselst:
+            print(str(expenselst.index(i)) + " " + i.getCategory())
+# 6. Sum Income
+    elif Options == "5":
+        input_2 = input("Sum up income? [yes/no]: ")
+        if input_2 == "yes":
+            k = 0
+            for i in income_list:
+                k += i.amount
+            print("The sum is:", "$", k)
+        elif input_2 == "no":
+            print("Why the fuck did you choose me then?")
+# 7. Sum Expense
+    elif Options == "6":
+        inp_4 = input("Sum up expenses? [yes/no]: ")
+        if inp_4 == "yes":
+            b = 0
+            for i in expenselst:
+                b += i.price
+            print("The sum for expense is: $", + b)
+        elif inp_4 == "no":
+            print("Fuck you then! Why da fuck did you click on me?")
+        else:
+            print("Enter a valid answer!!")
+# 8. Delete Expense
+    elif Options == "7":
+        inp_2 = input("Delete expense? [yes/no]:")
+        if inp_2 == "yes":
+            for i in expenselst:
+                print((str(expenselst.index(i)) + " " + i.getCategory()))
+            inp_a = input("Which one?:")
+            del expenselst[int(inp_a)]
+        elif inp_2 == "no":
+            print("WHY did you click on me then?")
+# 9. Delete Income
+    elif Options == "8":
+        if input_3 == "yes":
+            for i in income_list:
+                print(str(income_list.index(i)) + " " + i.getCategory())
+            an_input = input("Which one?:")
+            del income_list[int(an_input)]
+        elif input_3 == "no":
+            print("Fuck you then! Why the fuck did you choose this option?")
+
+# 10. Get Total Net
+    elif Options == "9":
+        net = (k - b)
+        inp_5 = input("Get your net? [yes/no]: ")
+        if inp_5 == "yes":
+            print(net)
+        elif net > 0:
+            print("Congrats! On a monthly basis, you are saving ", "$", net)
+        elif net < 0:
+            print(
+                "Ouch. On a monthly basis, you're in the negative, with an integer of ", "$", net)
+
+    elif Options == None:
+        pass
